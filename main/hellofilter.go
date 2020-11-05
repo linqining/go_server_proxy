a579d744833f9103e586c530c4a8822a08c8e40b
linqining <891680283@qq.com>
2020-11-05 08:26:03 -0800

    迭代修改更换域名

--- main/hellofilter.go
+++ main/hellofilter.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-    "compress/gzip"
-    "github.com/PuerkitoBio/goquery"
-    "strings"
-    "fmt"
-    "github.com/zalando/skipper/filters"
-    "io"
-    "io/ioutil"
-    "regexp"
-    "log"
+	"compress/gzip"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/zalando/skipper/filters"
+	"io"
+	"io/ioutil"
+	"log"
+	"regexp"
+	"strings"
 )
 
-type helloSpec struct {}
+type helloSpec struct{}
 
 type helloFilter struct {
-    who string
+	who string
 }
 
 func (s *helloSpec) Name() string { return "hello" }
 
 func (s *helloSpec) CreateFilter(config []interface{}) (filters.Filter, error) {
-    if len(config) == 0 {
-        return nil, filters.ErrInvalidFilterParameters
-    }
+	if len(config) == 0 {
+		return nil, filters.ErrInvalidFilterParameters
+	}
 
-    if who, ok := config[0].(string); ok {
-        return &helloFilter{who}, nil
-    } else {
-        return nil, filters.ErrInvalidFilterParameters
-    }
+	if who, ok := config[0].(string); ok {
+		return &helloFilter{who}, nil
+	} else {
+		return nil, filters.ErrInvalidFilterParameters
+	}
 }
 
 func (f *helloFilter) Request(ctx filters.FilterContext) {
@@ -37,81 +37,104 @@ func (f *helloFilter) Request(ctx filters.FilterContext) {
 }
 
 func (f *helloFilter) Response(ctx filters.FilterContext) {
-    request_url := ctx.Request().URL
-    app_reg := regexp.MustCompile(".*app\\..*\\.js")
+	request_url := ctx.Request().URL
+	app_reg := regexp.MustCompile(".*app\\..*\\.js")
 
-    if app_reg.MatchString(request_url.Path){
-        bodyRes, err := readRes(ctx)
+	if app_reg.MatchString(request_url.Path) {
+		bodyRes, err := readRes(ctx)
 
-        if err!=nil{
-            fmt.Printf("读取错误%s", err)
-        }
+		if err != nil {
+			fmt.Printf("读取错误%s", err)
+		}
 
-        replace_str := replaceDomain(bodyRes)
+		replace_str := replaceDomain(bodyRes)
 
-        _, werr:=ctx.ResponseWriter().Write(replace_str)
-        if werr!=nil{
-            log.Print(werr)
-        }
-    }
+		_, werr := ctx.ResponseWriter().Write(replace_str)
+		if werr != nil {
+			log.Print(werr)
+		}
+	}
 
-    if ctx.Response().Header.Get("Content-Type")=="text/html" {
-        bodyRes, err := readRes(ctx)
+	chunk_reg := regexp.MustCompile(".*chunk.*\\.js")
 
-        fmt.Println(ctx.Response().Header.Get("Content-Type"))
+	if chunk_reg.MatchString(request_url.Path) {
+		bodyRes, err := readRes(ctx)
+		if err != nil {
+			fmt.Printf("读取错误%s", err)
+		}
 
-        if err!=nil{
-            fmt.Printf("读取错误%s", err)
-        }
+		replace_str := replaceHost(bodyRes)
 
-        replace_str := replaceDomain(bodyRes)
+		_, werr := ctx.ResponseWriter().Write(replace_str)
+		if werr != nil {
+			log.Print(werr)
+		}
+	}
 
-        doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(replace_str)))
-        if err!=nil{
+	if ctx.Response().Header.Get("Content-Type") == "text/html" {
+		bodyRes, err := readRes(ctx)
 
-        }
-        if doc!=nil{
-            head := doc.Find("head")
-            head.AppendHtml(customCSS())
-            head.Find("title").SetText("法律咨询")
-        }
-        var result string
-        result, _ = doc.Html()
+		fmt.Println(ctx.Response().Header.Get("Content-Type"))
 
-        _, err =ctx.ResponseWriter().Write([]byte(result))
-        if err!=nil{
-            log.Print(err)
-        }
-    }
+		if err != nil {
+			fmt.Printf("读取错误%s", err)
+		}
+
+		replace_str := replaceDomain(bodyRes)
+
+		doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(replace_str)))
+		if err != nil {
+
+		}
+		if doc != nil {
+			head := doc.Find("head")
+			head.AppendHtml(customCSS())
+			head.Find("title").SetText("法律咨询")
+		}
+		var result string
+		result, _ = doc.Html()
+
+		_, err = ctx.ResponseWriter().Write([]byte(result))
+		if err != nil {
+			log.Print(err)
+		}
+	}
 }
 
 func replaceDomain(res []byte) []byte {
-    rep_host := Config.Domain+":"+Config.Port
-    reg := regexp.MustCompile("ai.12348.gov.cn")
-    replace_str := reg.ReplaceAll(res,[]byte(rep_host))
+	rep_host := Config.Domain + ":" + Config.Port
+	reg := regexp.MustCompile("ai.12348.gov.cn")
+	replace_str := reg.ReplaceAll(res, []byte(rep_host))
 
-    reg = regexp.MustCompile("hrpay.laway.cn")
-    replace_str = reg.ReplaceAll(replace_str,[]byte(rep_host+"/hrpay"))
+	reg = regexp.MustCompile("hrpay.laway.cn")
+	replace_str = reg.ReplaceAll(replace_str, []byte(rep_host+"/hrpay"))
 
-    reg = regexp.MustCompile("newsystem.laway.cn")
-    replace_str = reg.ReplaceAll(replace_str,[]byte(rep_host+"/newsystem"))
+	reg = regexp.MustCompile("newsystem.laway.cn")
+	replace_str = reg.ReplaceAll(replace_str, []byte(rep_host+"/newsystem"))
 
-    return replace_str
+	return replace_str
 }
 
-func readRes(ctx filters.FilterContext)([]byte,error){
-    var reader io.ReadCloser
-    var err error
-    if ctx.Response().Header.Get("Content-Encoding") == "gzip" {
-        reader, err = gzip.NewReader(ctx.Response().Body)
-    } else {
-        reader = ctx.Response().Body
-    }
-    bodyRes, err := ioutil.ReadAll(reader)
-    return bodyRes, err
+func replaceHost(res []byte) []byte {
+	rep_host := "\"https://ai.12348.gov.cn\""
+	reg := regexp.MustCompile("window.location.origin")
+	replace_str := reg.ReplaceAll(res, []byte(rep_host))
+	return replace_str
 }
 
-func customCSS()string{
-    return "<style>.advisory-info-main,.page-header,.pc-header,.pc-footer,.page-header-con,.footer-container,.advisory-main li:nth-child(n+17){display:none!important}</style>"+
-        "<style>.page-header-home,.page-banner,.tec,.advisory-icon-list li:nth-child(n+17){display:none !important}</style>"
+func readRes(ctx filters.FilterContext) ([]byte, error) {
+	var reader io.ReadCloser
+	var err error
+	if ctx.Response().Header.Get("Content-Encoding") == "gzip" {
+		reader, err = gzip.NewReader(ctx.Response().Body)
+	} else {
+		reader = ctx.Response().Body
+	}
+	bodyRes, err := ioutil.ReadAll(reader)
+	return bodyRes, err
+}
+
+func customCSS() string {
+	return "<style>.advisory-info-main,.page-header,.pc-header,.pc-footer,.page-header-con,.footer-container,.advisory-main li:nth-child(n+17){display:none!important}</style>" +
+		"<style>.page-header-home,.page-banner,.tec,.advisory-icon-list li:nth-child(n+17){display:none !important}</style>"
 }
